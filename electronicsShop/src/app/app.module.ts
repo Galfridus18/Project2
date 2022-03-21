7281486e9592630e6f3d1a41290d74d270af46b7
Angeudy Brisita <angeudybp@gmail.com>
2022-03-20 22:34:45 -0400

    update
    Added navbar and homepage templates

--- electronicsShop/src/app/app.module.ts
+++ electronicsShop/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContainerComponent } from './components/container/container.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContainerComponent,
+    
   ],
   imports: [
     BrowserModule,
