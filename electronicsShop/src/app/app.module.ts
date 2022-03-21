07f8dcc4a9e1eac79ba3b276bd01b3f4fbb38b10
Angeudy Brisita <angeudybp@gmail.com>
2022-03-21 14:36:03 -0400

    updated bootstrap
    bootstrap and carousel implemented

--- electronicsShop/src/app/app.module.ts
+++ electronicsShop/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +11,10 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContainerComponent } from './components/container/container.component';
+import { ProductsComponent } from './components/products/products.component';
+
+
+
 
 
 @NgModule({
@@ -18,11 +25,15 @@ import { ContainerComponent } from './components/container/container.component';
     SidebarComponent,
     FooterComponent,
     ContainerComponent,
+    ProductsComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- electronicsShop/src/app/components/homepage/homepage.component.ts
+++ electronicsShop/src/app/components/homepage/homepage.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  providers: [NgbCarouselConfig]
+  
 })
 export class HomepageComponent implements OnInit {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  constructor() { }
+  constructor( config : NgbCarouselConfig) {
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 
+  
   ngOnInit(): void {
   }
 
